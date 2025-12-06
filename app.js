23ccee325163cf1973812cc0b3c20f1182f11e96
Anokhee Mepani <anokheemepani@Anokhees-MacBook-Air.local>
2025-12-06 15:34:13 -0500

    Fix feedings chart height by setting aspectRatio and maintainAspectRatio

--- app.js
+++ app.js
@@ -2691,7 +2691,8 @@ function updateChartFeedings() {
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false,
+      maintainAspectRatio: true,
+      aspectRatio: 2.5,
       plugins: {
         legend: {
           display: true,
